5cbff94bad63b1916491607f673e9a7ac227ccba
Diana <63475737+diapopa@users.noreply.github.com>
2020-10-01 14:19:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-#import plotly.express as px
+from dash.dependencies import Input, Output
+import plotly.express as px
 import pandas as pd
 
 
